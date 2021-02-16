98062bcd51b7e45ac9b2e353523aec329abab1b0
thangam7784@gmail.com <thangam7784@gmail.com>
2021-02-16 23:10:57 +0000

    restaurant v2.0

--- classwork/Javascript/displayEvens.js
+++ classwork/Javascript/displayEvens.js
@@ -4,52 +4,64 @@ var end = document.getElementById("end");
 var step = document.getElementById("step");
 var results = document.getElementById("results");
 var submitButton = document.getElementById("submitButton");
+var outputField = document.getElementById("outputText");
+var outputTitleField = document.getElementById("outputTitle");
+var outputTextField = document.getElementById("outputText");
 
 function displayEvens() 
 {
-    var begin   = parseInt(start.value, 10);
-	if(Number.isNaN(begin))
+	var error = false;
+	var message = "";
+	var begin   = parseInt(start.value, 10);
+	if (Number.isNaN(begin))
 	{
-		alert("Starting Number should be Numeric");
-		 return false;
+		error=true;
+		message += "\n" + "Starting Number should be Numeric";
 	}
     var finish  = parseInt(end.value, 10);
-	if(Number.isNaN(finish))
+	if (Number.isNaN(finish))
 	{
-		alert("Ending Number should be Numeric");
-		return false;
+		message += "\n"+"Ending Number should be Numeric";
+		error=true;
 	} 
-	else if(finish<=begin)
+	else if (finish<=begin)
 	{
-		alert("End Number should be greater or equal to Start Number");
-		return false;
-	} 
+		message += "\n"+"End Number should be greater or equal to Start Number";
+		error = true;
+	}	
 	var count = parseInt(step.value, 10);
-	if(Number.isNaN(count))
+	if (Number.isNaN(count))
 	{
-		alert("Step should be Numeric");
-		return false;
+		message += "\n"+"Step should be Numeric";
+		error=true;
 	}
-	else if(count<0)
+	else if (count<0)
 	{
-		alert("Step is not a Negative Number ");
-		return false;
+		message += "\n"+"Step should not a Negative Number ";
+		error=true;
 	}
-	var i=[];
 	
-	var output = "";
-    for( i=begin;i<=finish;i=i+count)
+	outputTitleField.innerText="Here are the Even Numbers Between " + start.value + " and " + end.value + " by " + step.value + " 's' ";
+    
+	if(error==true)
 	{
-		if(i%2==0)
-		{
-			output += i+"\n";
-		}
+		outputField.innerText=message;
+		outputField.style.color = "RED";
 	}
-	//results.style.display = "block";
-	document.getElementById("outputTitle").innerText='Here are the Even Numbers Between ${begin} and ${finish} by ${count}: ';
-	document.getElementById("outputText").innerText=output;
-
-    // We always return false so that the form doesn't submit.
-    // Submission causes the page to reload.
-    return false;
+	else
+	{
+		var i;
+	
+		var output = "";
+		for( i=begin;i<=finish;i=i+count)
+		{
+			if(i%2==0)
+			{
+				output += i+"\n";
+			}
+		}
+		outputTextField.innerText=output;
+	}
+	    
+    return;
 }
